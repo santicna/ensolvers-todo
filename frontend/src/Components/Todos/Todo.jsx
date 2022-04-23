138647f6e8019218ef47be72c2988f7c03cde049
Santiago Cartagena <scartagena147@gmail.com>
2022-04-23 12:05:54 -0300

    Phase one complete

--- frontend/src/Components/Todos/Todo.jsx
+++ frontend/src/Components/Todos/Todo.jsx
@@ -4,12 +4,11 @@ import todoContext from "./TodoContext";
 
 export const Todo = ({ id, text }) => {
   const context = useContext(todoContext);
-  console.log(`ACA TENES QUE VER EL TODO: ${text}`);
   return (
     <>
       <input type={"checkbox"} />
       <span>{text}</span>
-      <Link to={"/TodoEdit"}>
+      <Link to={"/TodoEdit"} state={{ id: id, text: text }}>
         <button>Edit</button>
       </Link>
       <button
--- frontend/src/Views/TodoView/TodoEditView/TodoEditView.jsx
+++ frontend/src/Views/TodoView/TodoEditView/TodoEditView.jsx
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 export const TodoEditView = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const id = location.state.id;
+  const text = location.state.text;
+
+  const [newText, setNewText] = useState(text);
+  const todos = JSON.parse(localStorage.getItem("todos"));
+
+  function handleInputChange(e) {
+    setNewText(e.target.value);
+  }
+
+  function editTodo(id, text) {
+    const editItem = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          id: id,
+          text: text.trim(),
+        };
+      }
+      return todo;
+    });
+    localStorage.setItem("todos", JSON.stringify(editItem));
+  }
   return (
     <>
-      <h1>Editing Task </h1>
-      <input name="todoedit" type="text" />
-      <button>Save</button>
-
-      <button>Cancel</button>
+      <h1>Editing Task "{text}"</h1>
+      <input
+        name="todoedit"
+        type="text"
+        value={newText}
+        onChange={handleInputChange}
+      />
+      <button
+        onClick={() => {
+          editTodo(id, newText);
+          navigate(-1);
+        }}
+      >
+        Save
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        Cancel
+      </button>
     </>
   );
 };
